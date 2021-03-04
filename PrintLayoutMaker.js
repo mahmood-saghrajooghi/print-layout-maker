fa9d0c50cb186dfc09692708e38de1ddc9ef5e3a
mahmoodsa <mahmoodsagharjooghi@gmail.com>
2021-03-05 02:09:08 +0330

    main chunk done and layout maker is working

---
+++ PrintLayoutMaker.js
@@ -0,0 +1,94 @@
+class Page {
+  constructor(props) {
+    this.layout = props.layout;
+    this.screenDPI = 96;
+    this.calcHeight()
+    this.preparePage()
+    this.prepareBodyRows()
+    this.render();
+  }
+  preparePage() {
+    // A4 page size in centimeter is 21 cm x 29.7 cm
+    let pageWidth, pageHeight;
+    if (this.layout == 'portrait') {
+      pageWidth = 21;
+      pageHeight = 29.7;
+    } else if (this.layout == 'landscape') {
+      pageWidth = 29.7;
+      pageHeight = 21;
+    }
+    this.setPageDimensions({ pageWidth, pageHeight })
+  }
+
+  /**
+   * Sets Page Dimensions.
+   * @constructor
+   * @param {Number} pageWidth - Page width in centimeters
+   * @param {Number} PageHeight - Page height in centimeters
+   */
+  setPageDimensions(dimensions) {
+    const { pageWidth, pageHeight } = dimensions;
+    this.pageHeightInCm = pageHeight
+    this.pageHeightInPx = this.cmToPixel(pageHeight);
+    this.pageWidthInCm = pageWidth;
+    this.pageWidthInPx = this.cmToPixel(pageWidth)
+  }
+
+  calcHeight() {
+    this.header = document.querySelector('[data-role="header"]');
+    this.headerHeight = this.header.offsetHeight;
+    this.footer = document.querySelector('[data-role="footer"]')
+    this.footerHeight = this.footer.offsetHeight;
+    this.body = document.querySelector('[data-role="body"]')
+    this.bodyHeight = this.body.offsetHeight;
+    this.documentHeight = this.headerHeight + this.bodyHeight + this.footerHeight
+  }
+
+  prepareBodyRows() {
+    this.rows = [''];
+    let rowIndex = 0;
+    let bodyHeightLimit = this.pageHeightInPx - this.headerHeight - this.footerHeight;
+    let totalHeight = 0;
+    for (let node of this.body.children) {
+      totalHeight += node.offsetHeight;
+      if (totalHeight > bodyHeightLimit) {
+        totalHeight = node.offsetHeight;
+        rowIndex++;
+        this.rows[rowIndex] = ''
+      }
+      this.rows[rowIndex] += node.outerHTML
+    }
+  }
+
+  render() {
+    document.body.innerHTML = ''
+    this.rows.forEach(rowLayout => {
+      // console.log(rowLayout);
+      const a = `<table class="table-1 bordered" style="width: 100%;direction: rtl;"><tbody style="direction:rtl;">`
+      const c = `</tbody></table>`
+      const d = `<div class="container-offical" style="font-size:13px"><div class="box">`
+      const e = `</div></div>`
+      const pageLayout = d + this.header.innerHTML + a + rowLayout + c + this.footer.innerHTML + e;
+      const b = new DOMParser().parseFromString(pageLayout, 'text/html').body
+      b.style.width = this.pageWidthInPx
+      b.style.height = this.pageHeightInPx
+      // console.log(b);
+      document.body.appendChild(b)
+    })
+  }
+
+  cmToPixel(cm) {
+    return cm * this.screenDPI / 2.54;
+  }
+  print() {
+    window.print();
+  }
+}
+
+
+document.querySelector('[data-role="print"]').addEventListener('click', function () {
+  const page = new Page({
+    layout: 'landscape'
+  });
+  page.print();
+})
